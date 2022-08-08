6da7adc04e5c03afeb9fa7d5acd54ca132b8d6d4
Louis Lefebvre <lefebl4@medtronic.com>
2022-08-08 15:51:30 -0500

    add article functionality

--- main.go
+++ main.go
@@ -8,17 +8,6 @@ import (
 
 var router *gin.Engine
 
-type article struct {
-	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-}
-
-var articleList = []article{
-	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
-	{ID: 2, Title: "Article 2", Content: "Article 2 body"},
-}
-
 func showIndexPage(c *gin.Context) {
 	c.HTML(http.StatusOK,
 		"index.html",
